01e85ed937dfb7f37062d5ae98c942e5adf4df1d
Souvik Jana <souvikjana2013@gmail.com>
2023-06-04 11:34:07 +0530

    feat: add reset functionality in progress bar

--- Progress-bar/index.js
+++ Progress-bar/index.js
@@ -1,5 +1,6 @@
 const startBtn = document.querySelector(".start-btn");
 const stopBtn = document.querySelector(".stop-btn");
+const resetBtn = document.querySelector(".reset-btn");
 const progressBar = document.querySelector(".progress-bar");
 const progress = document.querySelector(".progress");
 let requestFrameId;
@@ -18,5 +19,25 @@ const stopProgressing = () => {
   cancelAnimationFrame(requestFrameId);
 };
 
-startBtn.addEventListener("click", startProgressing);
-stopBtn.addEventListener("click", stopProgressing);
+const startClickHandler = (e) => {
+  e.target.disabled = true;
+  stopBtn.disabled = false;
+  startProgressing();
+};
+
+const stopClickHandler = (e) => {
+  e.target.disabled = true;
+  startBtn.disabled = false;
+  stopProgressing();
+};
+
+const resetClickHandler = () => {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  progress.style.width = "0%";
+  stopProgressing();
+};
+
+startBtn.addEventListener("click", startClickHandler);
+stopBtn.addEventListener("click", stopClickHandler);
+resetBtn.addEventListener("click", resetClickHandler);
