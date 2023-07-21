a432b9b5505c3d1616c6f02852806e7d7a02ab1e
Souvik Jana <souvikjana2013@gmail.com>
2023-07-21 22:06:08 +0530

    feat: update month view based on the selected month and year

--- Calendar/js/Calendar.js
+++ Calendar/js/Calendar.js
@@ -2,22 +2,68 @@ import {
   getElementFromHtml,
   getDays,
   days,
+  months,
   get1stDayOfMonth,
 } from "./utils.js";
 
 export class Calendar {
-  constructor(month, year) {
-    this.container = getElementFromHtml(`<div class="month"></div>`);
+  constructor() {
+    const calendarHtml = `
+        <div class="container">
+            <div class="controls"></div>
+            <div class="month"></div>
+        </div>
+    `;
+    this.container = getElementFromHtml(calendarHtml);
+    this.monthView = this.container.querySelector(".month");
+    this.controls = this.container.querySelector(".controls");
 
-    this.init(month, year);
+    this.selectedMonth = months[new Date().getMonth()];
+    this.selectedYear = new Date().getFullYear();
+
+    this.renderControls();
+    this.renderMonthView(this.selectedMonth, this.selectedYear);
   }
 
-  init(month, year) {
-    const numberOfDays = getDays(month, year);
+  renderControls() {
+    let monthOptions = "";
+    for (const month of months) {
+      monthOptions += `<option value=${month}>${month}</option>`;
+    }
 
-    const day1 = get1stDayOfMonth(month, year);
-    console.log(day1);
+    const monthSelect = `<select>${monthOptions}</select>`;
+    this.monthControl = getElementFromHtml(monthSelect);
+    this.controls.appendChild(this.monthControl);
+    this.monthControl.value = this.selectedMonth;
 
+    const currentYear = new Date().getFullYear();
+
+    let yearOptions = "";
+    for (let i = currentYear - 30; i <= currentYear + 30; i++) {
+      yearOptions += `<option value=${i}>${i}</option>`;
+    }
+
+    const yearSelect = `<select>${yearOptions}</select>`;
+    this.yearControl = getElementFromHtml(yearSelect);
+    this.controls.appendChild(this.yearControl);
+    this.yearControl.value = this.selectedYear;
+
+    this.addEvents();
+  }
+
+  addEvents() {
+    this.monthControl.addEventListener("change", (e) => {
+      this.selectedMonth = e.target.value;
+      this.renderMonthView(this.selectedMonth, this.selectedYear);
+    });
+
+    this.yearControl.addEventListener("change", (e) => {
+      this.selectedYear = e.target.value;
+      this.renderMonthView(this.selectedMonth, this.selectedYear);
+    });
+  }
+
+  renderDays() {
     const daysFragment = document.createDocumentFragment();
 
     for (const day of days) {
@@ -25,22 +71,32 @@ export class Calendar {
       daysFragment.appendChild(dayElement);
     }
 
-    this.container.appendChild(daysFragment);
+    this.monthView.appendChild(daysFragment);
+  }
+
+  renderMonthView(month, year) {
+    this.monthView.innerHTML = "";
+
+    this.renderDays();
+
+    const indexOfMonth = months.indexOf(month);
+    const numberOfDays = getDays(indexOfMonth, year);
+    const day1 = get1stDayOfMonth(indexOfMonth, year);
 
     const gapFragment = document.createDocumentFragment();
     for (let i = 1; i <= day1; i++) {
       gapFragment.appendChild(getElementFromHtml(`<div></div>`));
     }
 
-    this.container.appendChild(gapFragment);
+    this.monthView.appendChild(gapFragment);
 
-    const fragment = document.createDocumentFragment();
+    const dateFragment = document.createDocumentFragment();
 
     for (let i = 1; i <= numberOfDays; i++) {
       const dateElement = getElementFromHtml(`<div>${i}</div>`);
-      fragment.appendChild(dateElement);
+      dateFragment.appendChild(dateElement);
     }
 
-    this.container.appendChild(fragment);
+    this.monthView.appendChild(dateFragment);
   }
 }
--- Calendar/js/index.js
+++ Calendar/js/index.js
@@ -1,11 +1,6 @@
 import { Calendar } from "./Calendar.js";
-import { months } from "./utils.js";
 
-const month = "Jul",
-  year = 2023;
-const indexOfMonth = months.indexOf(month);
-
-const calendar = new Calendar(indexOfMonth, year);
+const calendar = new Calendar();
 
 const root = document.getElementById("root");
 root.appendChild(calendar.container);
