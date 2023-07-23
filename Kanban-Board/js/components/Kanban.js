ab8e72222b602d8c4ed45e47869161aaa2793bfd
Souvik Jana <souvikjana2013@gmail.com>
2023-07-24 00:44:37 +0530

    feat: add support for light & dark theme

--- Kanban-Board/js/components/Kanban.js
+++ Kanban-Board/js/components/Kanban.js
@@ -12,6 +12,39 @@ export class Kanban {
     });
 
     this.root.appendChild(columns);
+    this.addEvents();
+    this.loadTheme();
+  }
+
+  addEvents() {
+    this.themeStyleLink = document.getElementById("themeStyleLink");
+    this.themeBtn = document.querySelector(".theme-btn");
+
+    this.themeBtn.addEventListener("click", () => {
+      const currentTheme = localStorage.getItem("theme") || "light";
+      if (currentTheme === "light") {
+        this.updateTheme("dark");
+        this.themeBtn.textContent = "Light";
+      } else {
+        this.updateTheme("light");
+        this.themeBtn.textContent = "Dark";
+      }
+    });
+  }
+
+  loadTheme() {
+    const currentTheme = localStorage.getItem("theme") || "light";
+    this.updateTheme(currentTheme);
+    if (currentTheme === "light") {
+      this.themeBtn.textContent = "Dark";
+    } else {
+      this.themeBtn.textContent = "Light";
+    }
+  }
+
+  updateTheme(themeName) {
+    this.themeStyleLink.setAttribute("href", `css/themes/${themeName}.css`);
+    localStorage.setItem("theme", themeName);
   }
 
   static get columns() {
