496260557fdfca3cee86138a71384a42bef68df7
Souvik Jana <souvikjana2013@gmail.com>
2023-09-10 21:01:53 +0530

    feat: add changes to mark mail as read or unread

--- Email/src/js/MailList.js
+++ Email/src/js/MailList.js
@@ -8,6 +8,7 @@ let mails = [
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere nostrum blanditiis itaque ipsam aliquid a quos, quam perspiciatis accusantium id inventore eos nemo dicta provident dolorum! Sint quam blanditiis aliquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt dolore molestiae adipisci illo corporis et alias quo. Error ducimus tempora consequatur quas cumque expedita tenetur dignissimos alias necessitatibus. Soluta, fuga. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, totam officiis. Voluptatum accusantium, placeat inventore libero fuga impedit quo vitae, nihil repudiandae nostrum expedita quos, facilis illo cum qui cupiditate. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente magnam inventore harum molestias esse natus numquam, provident quisquam, minima earum expedita labore soluta pariatur! Consectetur praesentium voluptatem voluptates sed aspernatur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, molestiae. Id accusamus voluptatem temporibus officia dolore necessitatibus praesentium, magni consectetur voluptates. Maxime ut molestiae hic, similique provident officia? Fuga, deleniti?",
     tags: ["inbox"],
+    isRead: false,
   },
   {
     id: crypto.randomUUID(),
@@ -15,6 +16,7 @@ let mails = [
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere nostrum blanditiis itaque ipsam aliquid a quos, quam perspiciatis accusantium id inventore eos nemo dicta provident dolorum! Sint quam blanditiis aliquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt dolore molestiae adipisci illo corporis et alias quo. Error ducimus tempora consequatur quas cumque expedita tenetur dignissimos alias necessitatibus. Soluta, fuga. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, totam officiis. Voluptatum accusantium, placeat inventore libero fuga impedit quo vitae, nihil repudiandae nostrum expedita quos, facilis illo cum qui cupiditate. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente magnam inventore harum molestias esse natus numquam, provident quisquam, minima earum expedita labore soluta pariatur! Consectetur praesentium voluptatem voluptates sed aspernatur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, molestiae. Id accusamus voluptatem temporibus officia dolore necessitatibus praesentium, magni consectetur voluptates. Maxime ut molestiae hic, similique provident officia? Fuga, deleniti?",
     tags: ["inbox", "important"],
+    isRead: true,
   },
 ];
 
@@ -33,11 +35,16 @@ export class MailList {
     this.elements.mailDetails = getElementFromHtml(
       `<section class="mail-details"></section>`
     );
-    this.elements.mailDetails.innerHTML = "";
     root.append(this.elements.mailDetails);
 
+    this.resetMailDetailsSection();
     this.renderMails(emailType);
     this.addEvents();
+  }
+
+  resetMailDetailsSection() {
+    this.elements.mailDetails.innerHTML = "";
+    this.openedMailId = "";
   }
 
   renderMails(emailType) {
@@ -48,11 +55,15 @@ export class MailList {
       const markedAsImportant = mail.tags.some((tag) => tag === "important");
 
       const mailEl = `
-        <li class="mail" data-mail-id=${mail.id}>            
-            <h2>${mail.subject}</h2>
+        <li class="mail ${!mail.isRead ? "unread" : ""}" data-mail-id=${
+        mail.id
+      }>            
+            <h2 class="mail-subject">${mail.subject}</h2>
             <p class="line-2">${mail.content}</p> 
             <section class="actions">
-              <button data-action="read">Read</button>           
+              <button data-action="read">${
+                mail.isRead ? "Unread" : "Read"
+              }</button>           
               <button data-action="important">${
                 markedAsImportant ? "Marked as Important" : "Mark as important"
               }</button>           
@@ -77,19 +88,52 @@ export class MailList {
           selectedMailId,
         });
         return;
+      } else if (e.target.dataset.action === "read") {
+        this.handleMarkAsRead({
+          targetElement: e.target,
+          selectedMail,
+          selectedMailId,
+        });
+        return;
       }
 
-      const selectedMailDetails = mails.find(
-        (mail) => mail.id === selectedMailId
-      );
+      if (selectedMail) {
+        const selectedMailDetails = mails.find(
+          (mail) => mail.id === selectedMailId
+        );
 
-      this.selectedMail?.classList.remove("active");
-      this.selectedMail = selectedMail;
-      selectedMail.classList.add("active");
-      this.elements.mailDetails.innerHTML = new MailDetails(
-        selectedMailDetails
-      ).root;
+        this.selectedMail?.classList.remove("active");
+        this.selectedMail = selectedMail;
+        selectedMail?.classList.add("active");
+        this.openedMailId = selectedMailId;
+        this.elements.mailDetails.innerHTML = new MailDetails(
+          selectedMailDetails
+        ).root;
+
+        if (!selectedMailDetails.isRead) {
+          this.handleMarkAsRead({
+            targetElement: selectedMail.querySelector(
+              "button[data-action='read']"
+            ),
+            selectedMail,
+            selectedMailId,
+          });
+        }
+      }
     });
+  }
+
+  handleMarkAsRead({ targetElement, selectedMail, selectedMailId }) {
+    selectedMail.classList.toggle("unread");
+    mails = mails.map((mail) =>
+      mail.id === selectedMailId ? { ...mail, isRead: !mail.isRead } : mail
+    );
+    const isMailRead = targetElement.textContent.toLowerCase() === "read";
+    targetElement.textContent = isMailRead ? "Unread" : "Read";
+
+    if (!isMailRead && this.openedMailId === selectedMailId) {
+      this.resetMailDetailsSection();
+    }
   }
 
   handleMarkAsImportant({ targetElement, selectedMail, selectedMailId }) {
@@ -113,7 +157,7 @@ export class MailList {
       targetElement.textContent = "Mark as important";
       if (location.pathname.includes("important")) {
         selectedMail.remove();
-        this.elements.mailDetails.innerHTML = "";
+        this.resetMailDetailsSection();
       }
     }
   }
